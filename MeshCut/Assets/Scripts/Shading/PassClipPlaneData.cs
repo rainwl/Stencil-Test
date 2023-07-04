c815f00abc4000769d6a84bb4ba2f8090f813eef
Rain <2215338912@qq.com>
2023-07-04 22:22:23 +0800

    Add:Shading way

---
+++ MeshCut/Assets/Scripts/Shading/PassClipPlaneData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PassClipPlaneData : MonoBehaviour
+{
+    public Transform clipPlane;
+    public Material clipByPlaneMat;
+
+    private void Update()
+    {
+        if (clipPlane == null || !clipByPlaneMat) return;
+        clipByPlaneMat.SetVector("_PlanePos",clipPlane.position);
+        clipByPlaneMat.SetVector("_PlaneNormal", clipPlane.up);
+    }
+}
